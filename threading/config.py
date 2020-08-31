0416e4bc895c74d529f41a9534802ec09f59b131
Zhang Yongzhe <jcddyongzhe@gmial.com>
2020-08-31 19:51:12 +0800

    Pc comm thread: server and client

---
+++ threading/config.py
@@ -0,0 +1,10 @@
+#for pc
+WIFI_IP = "10.27.167.70" # 192.168.27.27 Connecting to IP address of MDPGrp27
+WIFI_PORT = 5182
+#for tablet
+N7_MAC = "68:B3:5E:58:97:28"
+#for arduino
+BAUD = 115200
+UUID = "00001101-0000-1000-8000-00805F9B34FB"
+SER_PORT0 = "/dev/ttyACM0"
+SER_PORT1 = "/dev/ttyACM1"
---
+++ threading/pcClient.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+
+import socket
+
+HOST = '10.27.167.70'  # The server's hostname or IP address
+PORT = 5182        # The port used by the server
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall(b'Hello, world')
+#used to  terminate transmission
+#s.sendall(b'done')
+data = s.recv(1024)
+
+print'Received %s' %repr(data)
