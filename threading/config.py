fe8e611a642b44a68302edd53f3e5689937a4959
Zhang Yongzhe <jcddyongzhe@gmial.com>
2020-09-03 20:01:21 +0800

    Update config.py

--- threading/config.py
+++ threading/config.py
@@ -1,5 +1,5 @@
 #for pc
-WIFI_IP = "10.27.167.70" # 192.168.27.27 Connecting to IP address of MDPGrp27
+WIFI_IP = "192.168.27.27" #Connecting to IP address of MDPGrp27
 WIFI_PORT = 5182
 #for tablet
 N7_MAC = "68:B3:5E:58:97:28"
@@ -7,4 +7,4 @@ N7_MAC = "68:B3:5E:58:97:28"
 BAUD = 115200
 UUID = "00001101-0000-1000-8000-00805F9B34FB"
 SER_PORT0 = "/dev/ttyACM0"
-SER_PORT1 = "/dev/ttyACM1"
+# SER_PORT1 = "/dev/ttyACM1"
