20a274a03e7932b5b7c49379e1f648e7e0a3bbe0
Zhang Yongzhe <jcddyongzhe@gmial.com>
2020-09-03 20:24:13 +0800

    Update pcClient.py

--- threading/pcClient.py
+++ threading/pcClient.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python2
 import socket
-from config import WIFI_IP as serverIP
+from config import WIFI_IP as serverIP, WIFI_PORT as serverPort
 
 __author__ = "Zhang Y.Z."
 
 
 HOST = serverIP  # The server's hostname or IP address
-PORT = 5182        # The port used by the server
+PORT = serverPort        # The port used by the server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
