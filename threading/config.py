68d3ad0bc5ff968a3a6d0d2f9ad6bb38ed7ccd83
Zhang Yongzhe <jcddyongzhe@gmial.com>
2020-09-26 00:35:52 +0800

    update

--- threading/config.py
+++ threading/config.py
@@ -4,14 +4,13 @@ WIFI_PORT = 5182
 #for arduino
 BAUD = 115200
 SER_PORT0 = "/dev/ttyACM0"
-#for tablet
-N7_MAC = "68:B3:5E:58:97:28"
+#for tablet, need to put two devices near to each other 
+N7_MAC = "68:B3:5E:58:97:28"  
 phone_MAC = "44:78:3E:96:78:1E"
 LOCALE = 'UTF-8'
-RFCOMM_CHANNEL = 0
+RFCOMM_PORT = 4
 UUID = "00001101-0000-1000-8000-00805F9B34FB"    #UUID = '443559ba-b80f-4fb6-99d9-ddbcd6138fbd'
 ANDROID_SOCKET_BUFFER_SIZE = 512
 
-#for pc2
-# WIFI_IP2 = "192.168.27.225" #"192.168.27.27" #Connecting to IP address of MDPGrp27 192.168.27.225
-# WIFI_PORT2 = 5182
+#for RPi
+RPI_MAC = "B8:27:EB:DB:11:29"
