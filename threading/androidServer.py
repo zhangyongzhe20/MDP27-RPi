ce32e83d7cd91d55d9fe5f3031603776c93c410a
Zhang Yongzhe <jcddyongzhe@gmial.com>
2020-09-26 09:02:12 +0800

    update

--- threading/androidServer.py
+++ threading/androidServer.py
@@ -51,6 +51,7 @@ class AndroidAPI(object):
                                service_classes = [ UUID, SERIAL_PORT_CLASS ],
                                profiles = [ SERIAL_PORT_PROFILE ],
                                 )
+            print "6"
             print "Waiting for BT connection on RFCOMM channel %d" % self.port
             # Accept requests
             self.client_socket, client_address = self.server_socket.accept()
