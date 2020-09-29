1892ab27debbe92b055b2a86488c3669ac919c60
Zhang Yongzhe <jcddyongzhe@gmial.com>
2020-09-29 10:19:33 +0800

    Update version5.py

--- threading/mains/version5.py
+++ threading/mains/version5.py
@@ -50,7 +50,7 @@ class Main:
     def writeAndroid(self, Aqueue):
         while 1:
             if not Aqueue.empty():
-                msg = Aqueue.getnowait()
+                msg = Aqueue.get_nowait()
                 self.android.write(msg)
                 print "Write to android: %s\n" % msg
 
