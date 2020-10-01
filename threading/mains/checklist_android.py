5c74e5d71c0aca380c73bfdfec807424900bf0fb
Lester Tay <lester.tay@accenture.com>
2020-10-01 10:21:30 +0800

    changes to android checklist

--- threading/mains/checklist_android.py
+++ threading/mains/checklist_android.py
@@ -1,9 +1,6 @@
-from arduinoServer import robotAPI
 from androidServer import androidAPI
-from pcServer import pcAPI
 from config import *
 
-import Queue
 import thread
 import threading
 import os
@@ -22,15 +19,9 @@ class Main:
         self.android.connect()
         # second establish
         # third establish
-        self.robot.connect_serial()
 
-        print "All end-devices are connected\n"
+        print "Android connected\n"
 
-        # initialize queues
-        # self.Aqueue = Queue.Queue(maxsize=0)
-        # self.Rqueue = Queue.Queue(maxsize=0)
-        # self.Pqueue = Queue.Queue(maxsize=0)
-        # initialization done
 
     # read/write Android
     def readAndroid(self, Pqueue):
@@ -43,19 +34,19 @@ class Main:
     def writeAndroid(self, Aqueue):
         while 1:
             send_msg = raw_input()
-            print "Write(): %s " % send_msg
+            
             if send_msg:
               self.android.write(send_msg)
-              print "Write to android: %s\n" % msg
+              print "Write to android: %s\n" % send_msg
 
     # read/write Robot
 
     def Mthreads(self):
         try:
             # 1: Read from android
-            thread.start_new_thread(self.readAndroid, (self.Pqueue,))
+            thread.start_new_thread(self.readAndroid)
             # 5: Write to Android
-            thread.start_new_thread(self.writeAndroid, (self.Aqueue,))
+            thread.start_new_thread(self.writeAndroid)
 
         except Exception, e:
             # print "Error in mode %s: %s" % mode % str(e)
